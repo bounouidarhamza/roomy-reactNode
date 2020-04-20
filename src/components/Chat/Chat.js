5d8d6da525331808d79fe969becf9366f5409e52
hbounouidar <bounouidarhamza@gmail.com>
2020-04-20 19:09:43 +0200

    deployin react

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -17,7 +17,8 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
+ // const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
+  const ENDPOINT = 'https://roomy-nodereact-git-deploy-react.hbo.now.sh/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
