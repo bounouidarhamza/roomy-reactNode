2fa759c7bacdf40bc20610850c6a623129038529
hbounouidar <bounouidarhamza@gmail.com>
2020-04-20 18:38:57 +0200

    zeit config

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -14,7 +14,7 @@ const Join = () => {
           <h1 className="heading">Roomy</h1>
           <div>
             <input
-              placeholder="Name"
+              placeholder="Name" 
               className="joinInput"
               type="text"
               onChange={(event) => setName(event.target.value)}
